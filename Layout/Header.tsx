9c803eb7924f525372e6b7e7a9ae9b560b157ab0
Lewis Rotchell <rotchell92@gmail.com>
2021-09-09 17:51:52 +0100

    Added focus trap on side bar

--- Layout/Header.tsx
+++ Layout/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Hamburger } from "../components/Hamburger";
-import { ThemeContext } from "../context/ThemeProvider";
 import classes from "./Header.module.scss";
+import FocusTrap from "focus-trap-react";
 
 interface HeaderProps {}
 
@@ -10,42 +10,47 @@ export const Header: React.FC<HeaderProps> = ({}) => {
 
   useEffect(() => {
     const main = document.querySelector("main");
+    const body = document.querySelector("body");
     if (active) {
       main?.classList.add("active");
       main?.addEventListener("click", () => {
         main.classList.remove("active");
         setActive(false);
       });
+      body?.classList.add("stop-scroll");
     } else if (!active && main?.classList.contains("active")) {
       main?.classList.remove("active");
+      body?.classList.remove("stop-scroll");
     }
   });
 
   return (
-    <>
-      <nav className={`${classes.nav} container`}>
-        {/* <button onClick={() => setActive(!active)}>menu</button> */}
-        <Hamburger active={active} setActive={setActive} />
-      </nav>
-
-      <aside className={`${classes.aside} ${active ? classes.active : ""}`}>
-        <nav>
-          <ul>
-            <li>
-              <a href="#home">Home</a>
-            </li>
-            <li>
-              <a href="#about">About</a>
-            </li>
-            <li>
-              <a href="#projects">Projects</a>
-            </li>
-            <li>
-              <a href="#contact">Contact</a>
-            </li>
-          </ul>
+    <FocusTrap active={active} focusTrapOptions={{}}>
+      <div>
+        <nav className={`${classes.nav} container`}>
+          {/* <button onClick={() => setActive(!active)}>menu</button> */}
+          <Hamburger active={active} setActive={setActive} />
         </nav>
-      </aside>
-    </>
+
+        <aside className={`${classes.aside} ${active ? classes.active : ""}`}>
+          <nav>
+            <ul>
+              <li>
+                <a href="#home">Home</a>
+              </li>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#projects">Projects</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </aside>
+      </div>
+    </FocusTrap>
   );
 };
