7a7bcb3f416034cd1e971eb80dd9e2997f559dd5
Lewis Rotchell <rotchell92@gmail.com>
2021-09-21 13:34:35 +0100

    Added styling to contact form

--- client/components/Button.tsx
+++ client/components/Button.tsx
@@ -5,13 +5,22 @@ interface ButtonProps {
   children: string;
   style?: string;
   linkTo?: string;
+  type?: string;
 }
 
 export const Button: React.FC<ButtonProps> = ({
   children,
   style,
   linkTo = "/",
+  type,
 }) => {
+  if ((type = "submit")) {
+    return (
+      <button type="submit" className={`${classes.button} ${style}`}>
+        {children}
+      </button>
+    );
+  }
   return (
     <Link href={linkTo}>
       <a className={`${classes.button} ${style}`}>{children}</a>
--- client/components/ContactForm.tsx
+++ client/components/ContactForm.tsx
@@ -2,6 +2,7 @@ import React, { InputHTMLAttributes } from "react";
 import classes from "./ContactForm.module.scss";
 import { Formik, useField, Form } from "Formik";
 import * as Yup from "yup";
+import { Button } from "./Button";
 
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string;
@@ -19,11 +20,11 @@ const InputField: React.FC<InputFieldProps> = ({ label, ...props }) => {
   const [field, meta] = useField(props);
 
   return (
-    <>
+    <div className={classes.formControl}>
       <label htmlFor={props.id || props.name}>{label}</label>
       <input className={classes.input} {...field} {...props}></input>
-      {meta.error ? <div className={classes.error}>{meta.error}</div> : null}
-    </>
+      {/* {meta.error ? <div className={classes.error}>{meta.error}</div> : null} */}
+    </div>
   );
 };
 
@@ -31,11 +32,11 @@ const TextArea: React.FC<TextAreaProps> = ({ label, ...props }) => {
   const [field, meta] = useField(props);
 
   return (
-    <>
+    <div className={classes.formControl}>
       <label htmlFor={props.id || props.name}>{label}</label>
       <textarea className={classes.textArea} {...field} {...props}></textarea>
-      {meta.error ? <div className={classes.error}>{meta.error}</div> : null}
-    </>
+      {/* {meta.error ? <div className={classes.error}>{meta.error}</div> : null} */}
+    </div>
   );
 };
 
@@ -63,21 +64,23 @@ export const ContactForm: React.FC<ContactForm> = ({}) => {
       }}
     >
       {({ isSubmitting }) => (
-        <Form>
-          <InputField name="name" label="name" type="text" placeholder="name" />
+        <Form className={classes.contactForm}>
+          <InputField name="name" label="Name" type="text" placeholder="Name" />
           <InputField
             name="email"
-            label="email"
+            label="Email"
             type="text"
-            placeholder="email"
+            placeholder="Email"
           />
           <TextArea
             name="message"
-            label="message"
+            label="Message"
             type="textarea"
-            placeholder="message"
+            placeholder="Message"
           />
-          <button type="submit">{isSubmitting ? "Loading" : "Submit"}</button>
+          <Button style={classes.button} type={"submit"}>
+            {isSubmitting ? "Loading" : "Submit"}
+          </Button>
         </Form>
       )}
     </Formik>
